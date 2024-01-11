28520e8a19ab70aeeb21477acbf040a916d5aaf7
Hardik C <138803612+HardikC08@users.noreply.github.com>
2024-01-12 00:01:02 +0530

    Positive numbers in list
    Write a program to print all positive numbers in a range using a list

---
+++ Positive Numbers Range.py
@@ -0,0 +1,8 @@
+list1 = [12,-7,5,64,-14]
+list2 = [12,14,-95,3]
+print("List1 (input) : " , list1)
+print("Output : " , list1[0], list1[2], list1[3])
+print("Arranging in ascending order (List1) : "  , list1[2], list1[0], list1[3])
+print("List2 (input) : " , list2)
+print("Output : " , list2[0], list2[1], list2[3])
+print("Arranging in ascending order (List2) : " , list2[3], list2[0], list2[1])
